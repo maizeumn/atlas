7f3a0a0a0ef4a0e2e0d272757f39338e8cecc318
orionzhou <zhoupenggeni@gmail.com>
2022-01-10 16:59:46 +0800

    updated DE and Dom test code

--- src/functions.R
+++ src/functions.R
@@ -3,7 +3,7 @@ require(devtools)
 load_all('~/git/rmaize')
 require(ggforce)
 require(ggpubr)
-require(ggExtra)
+#require(ggExtra)
 dira = '~/projects/assets'
 dirp = '~/projects/atlas'
 dird = file.path(dirp, 'data')
@@ -29,10 +29,10 @@ tissues23 = c("seedlingleaf_11DAS", "blade_v12", "flagleaf_0DAP",
             "embryo_27DAP", "embryo_imbibedseed",
             "endosperm_27DAP", "kernel_14DAP", "endosperm_14DAP")
 tiss23 = c("radicle_root", "seedling_root", "seedling_leaf", "seedling_meristem", "coleoptile", "auricle", "blade_leaf", "internode", "tassel", "sheath", "ear", "flag_leaf", "floret", "husk", "root", "silk", "spikelet", "tassel_stem", "endosperm14D", "kernel", "embryo", "seed_imbibed", "endosperm27D")
-tissues20 = tissues23[1:20]
-tissues2 = c("tasselstem_0DAP", "internode_v12")
+tiss20 = tissues23[1:20]
+tiss2 = c("tasselstem_0DAP", "internode_v12")
 gts3 = c("B73", "Mo17", 'BxM')
-tissues3 = c("seedling_root", "coleoptile", "leaf_V2")
+tiss3 = c("seedling_root", "coleoptile", "leaf_V2")
 gts10 = c("B73",'B84',"Mo17",'A682','B73xMo17','Mo17xB73',
           'B84xB73','B84xMo17','A682xB73','A682xMo17')
 #require(BiocParallel)
@@ -46,153 +46,6 @@ gts10 = c("B73",'B84',"Mo17",'A682','B73xMo17','Mo17xB73',
 
 cap_bigint <- function(x, int.max=2147483647) ifelse(x>int.max, int.max, x)
 trim_range <- function(x, range.min=-2147483647, range.max=2147483647) ifelse(x>range.max, range.max, ifelse(x<range.min, range.min, x))
-run_de_test <- function(tm1, th1) {
-    #{{{
-    require(DESeq2)
-    require(edgeR)
-    #{{{ prepare data
-    vh = th1 %>% mutate(Genotype = factor(Genotype)) %>% arrange(SampleID)
-    vh.d = column_to_rownames(as.data.frame(vh), var = 'SampleID')
-    gids = tm1 %>% group_by(gid) %>% summarise(n.sam = sum(ReadCount >= 10)) %>%
-        filter(n.sam > .2 * nrow(vh)) %>% pull(gid)
-    vm = tm1 %>% filter(gid %in% gids) %>%
-        select(SampleID, gid, ReadCount)
-    x = readcount_norm(vm)
-    mean.lib.size = mean(x$tl$libSize)
-    vm = x$tm
-    vm.w = vm %>% select(SampleID, gid, ReadCount) %>% spread(SampleID, ReadCount)
-    vm.d = column_to_rownames(as.data.frame(vm.w), var = 'gid')
-    stopifnot(identical(rownames(vh.d), colnames(vm.d)))
-    #{{{ hybrid vs mid-parent
-    hm = tm1 %>%
-        filter(gid %in% gids) %>%
-        select(SampleID, gid, nRC)
-    # prepare mid-parent
-    sids1 = th1 %>% filter(Genotype == 'B73') %>% pull(SampleID)
-    sids2 = th1 %>% filter(Genotype == 'Mo17') %>% pull(SampleID)
-    sidsh = th1 %>% filter(Genotype == 'BxM') %>% pull(SampleID)
-    if(length(sidsh)==0) sidsh = th1 %>% filter(Genotype == 'MxB') %>% pull(SampleID)
-    np1 = length(sids1); np2 = length(sids2)
-    if(np1 < np2) sids1 = c(sids1, sample(sids1, np2-np1, replace = T))
-    if(np1 > np2) sids2 = c(sids2, sample(sids2, np1-np2, replace = T))
-    tsi1 = tibble(p1 = sids1, p2 = sids2) %>%
-        mutate(sid = sprintf("mp%02d", 1:length(p1)))
-    tsi2 = expand.grid(sid = tsi1$sid, gid = gids) %>% as_tibble() %>%
-        mutate(sid = as.character(sid), gid = as.character(gid)) %>%
-        inner_join(tsi1, by = 'sid') %>%
-        inner_join(hm, by = c('p1'='SampleID','gid'='gid')) %>%
-        inner_join(hm, by = c('p2'='SampleID','gid'='gid')) %>%
-        transmute(SampleID = sid, gid = gid, nRC = (nRC.x+nRC.y)/2)
-    hm.w = hm %>% filter(SampleID %in% sidsh) %>%
-        bind_rows(tsi2) %>%
-        mutate(nRC = round(nRC)) %>%
-        spread(SampleID, nRC)
-    hm.d = column_to_rownames(as.data.frame(hm.w), var = 'gid')
-    #
-    gts.new = rep(c('MidParent','Hybrid'), c(nrow(tsi1),length(sidsh)))
-    hh = tibble(SampleID = c(tsi1$sid, sidsh), Genotype = gts.new) %>%
-        mutate(Genotype = factor(Genotype)) %>%
-        mutate(libSize = mean.lib.size, normFactor = 1) %>%
-        arrange(SampleID)
-    hh.d = column_to_rownames(as.data.frame(hh), var = 'SampleID')
-    stopifnot(identical(rownames(hh.d), colnames(hm.d)))
-    #}}}
-    #}}}
-    #{{{ DESeq2
-    dds = DESeqDataSetFromMatrix(countData=vm.d, colData=vh.d, design = ~0+Genotype)
-    sizeFactors(dds) = vh$sizeFactor
-    dds = estimateDispersions(dds, fitType = 'parametric')
-    disp = dispersions(dds)
-    #dds = nbinomLRT(dds, reduced = ~ 1)
-    dds = nbinomWaldTest(dds)
-    resultsNames(dds)
-    res1 = results(dds, contrast=c(-1,0,1), pAdjustMethod="fdr")
-    res2 = results(dds, contrast=c(-1,1,0), pAdjustMethod="fdr")
-    res3 = results(dds, contrast=c(0,1,-1), pAdjustMethod="fdr")
-    res4 = results(dds, contrast=c(-.5,1,-.5), pAdjustMethod="fdr")
-    stopifnot(rownames(res1) == gids)
-    stopifnot(rownames(res2) == gids)
-    stopifnot(rownames(res3) == gids)
-    stopifnot(rownames(res4) == gids)
-    # hvm
-    dds = DESeqDataSetFromMatrix(countData=hm.d, colData=hh.d, design = ~ Genotype)
-    sizeFactors(dds) = rep(1, nrow(hh))
-    dds = DESeq(dds, fitType = 'parametric')
-    resultsNames(dds)
-    res5 = results(dds, contrast=c("Genotype","Hybrid","MidParent"), pAdjustMethod="fdr")
-    stopifnot(rownames(res5) == gids)
-    #
-    t_ds = tibble(gid = gids, disp = disp,
-                padj.mb = res1$padj, log2mb = res1$log2FoldChange,
-                padj.hb = res2$padj, log2hb = res2$log2FoldChange,
-                padj.hm = res3$padj, log2hm = res3$log2FoldChange,
-                #padj.fm = res4$padj, log2fm = res4$log2FoldChange
-                padj.fm = res5$padj, log2fm = res5$log2FoldChange
-                ) %>%
-        replace_na(list(padj.mb = 1, padj.hb = 1, padj.hm = 1, padj.fm = 1))
-    #}}}
-    if(FALSE) {
-    #{{{ edgeR
-    y = DGEList(counts = vm.d, group = vh$Genotype)
-    y = calcNormFactors(y, method = 'TMM') #RLE
-    t_nf = y$samples %>% as_tibble() %>%
-        mutate(SampleID = rownames(y$samples)) %>%
-        select(SampleID = SampleID, libSize = lib.size, normFactor = norm.factors)
-    design = model.matrix(~0 + Genotype, data = vh)
-    colnames(design) = levels(vh$Genotype)
-    #y = estimateDisp(y, design)
-    y = estimateGLMCommonDisp(y, design, verbose = T)
-    y = estimateGLMTrendedDisp(y, design)
-    y = estimateGLMTagwiseDisp(y, design)
-    fit = glmFit(y, design)
-    t_cpm_merged = cpmByGroup(y) %>% as_tibble() %>%
-        transmute(cpm.b = B73, cpm.m = Mo17, cpm.h = BxM)
-    t_cpm = cpm(y, normalized.lib.sizes = T) %>% as_tibble() %>%
-        mutate(gid = gids) %>%
-        gather(sid, cpm, -gid) %>% select(sid, gid, cpm)
-    # mb, hb, hm, fm
-    lrt1 = glmLRT(fit, contrast = c(-1, 0, 1))
-    lrt2 = glmLRT(fit, contrast = c(-1, 1, 0))
-    lrt3 = glmLRT(fit, contrast = c(0, -1, 1))
-    lrt4 = glmLRT(fit, contrast = c(-.5, -.5, 1))
-    stopifnot(identical(gids, rownames(lrt1$table)))
-    stopifnot(identical(gids, rownames(lrt2$table)))
-    stopifnot(identical(gids, rownames(lrt3$table)))
-    stopifnot(identical(gids, rownames(lrt4$table)))
-    #tags = decideTestsDGE(lrt, adjust.method = "BH", p.value = .05, lfc = 1)
-    #stopifnot(identical(gids, rownames(tags)))
-    #{{{ fm
-    y = DGEList(counts = hm.d, lib.size = hh$libSize, norm.factors = hh$normFactor)
-    design = model.matrix(~0 + Genotype, data = hh)
-    colnames(design) = unique(hh$Genotype)
-    y = estimateGLMCommonDisp(y, design, verbose = T)
-    y = estimateGLMTrendedDisp(y, design)
-    y = estimateGLMTagwiseDisp(y, design)
-    fit = glmFit(y, design)
-    lrt5 = glmLRT(fit, contrast = c(-1, 1))
-    stopifnot(identical(gids, rownames(lrt5$table)))
-    #}}}
-    tr1 = lrt1$table %>% rownames_to_column("gid") %>% as_tibble() %>% 
-        mutate(padj.mb = p.adjust(PValue, method = 'BH')) %>%
-        select(gid, log2mb = logFC, padj.mb)
-    tr2 = lrt2$table %>% 
-        mutate(padj.hb = p.adjust(PValue, method = 'BH')) %>%
-        select(log2hb = logFC, padj.hb)
-    tr3 = lrt3$table %>%
-        mutate(padj.hm = p.adjust(PValue, method = 'BH')) %>%
-        select(log2hm = logFC, padj.hm)
-    tr4 = lrt4$table %>%
-        mutate(padj.fm = p.adjust(PValue, method = 'BH')) %>%
-        select(log2fm = logFC, padj.fm)
-    tr5 = lrt5$table %>%
-        mutate(padj.fm = p.adjust(PValue, method = 'BH')) %>%
-        select(log2fm = logFC, padj.fm)
-    t_eg = tr1 %>% bind_cols(tr2) %>% bind_cols(tr3) %>% bind_cols(tr5)
-    #}}}
-    }
-    list(deseq = t_ds, edger = '')
-    #}}}
-}
 plot_deseq2 <- function(dds, dirw, tissue) {
     #{{{
     require(DESeq2)
@@ -213,69 +66,6 @@ plot_deseq2 <- function(dds, dirw, tissue) {
     plotMA(resLFC, ylim = c(-3,3))
     dev.off()
     #}}}
-}
-call_de_dom <- function(t_de, tmm) {
-#{{{ call pDE, hDE, D/A, Dom
-tm1 = tmm %>% select(batch, Tissue, Genotype, gid, CPM) %>%
-    spread(Genotype, CPM) %>%
-    mutate(LP = pmin(B73, Mo17),
-           HP = pmax(B73, Mo17),
-           MP = (B73 + Mo17) / 2,
-           DoA = (BxM - MP) / (HP - MP)) %>%
-    select(batch, Tissue, gid, B73, Mo17, BxM, DoA) %>%
-    mutate(DoA = ifelse(is.nan(DoA)|is.infinite(DoA), NA, DoA))
-summary(tm1$DoA)
-#
-pDEs = c("DE_B", "DE_M", "non_DE")
-tm2 = t_de %>% select(batch, Tissue, deseq) %>% unnest(deseq) %>%
-    #replace_na(list(log2MB = 0, log2HB = 0, log2HM = 0, log2FM = 0)) %>%
-    mutate(tag.mb = ifelse(padj.mb < .01, ifelse(log2mb < 0, -1, 1), 0),
-           tag.hb = ifelse(padj.hb < .01, ifelse(log2hb < 0, -1, 1), 0),
-           tag.hm = ifelse(padj.hm < .01, ifelse(log2hm < 0, -1, 1), 0),
-           tag.fm = ifelse(padj.fm < .01, ifelse(log2fm < 0, -1, 1), 0)) %>%
-    mutate(pDE = ifelse(is.na(tag.mb), NA,
-                 ifelse(tag.mb == -1, 'DE_B',
-                 ifelse(tag.mb == 1, 'DE_M', 'non_DE')))) %>%
-    mutate(pDE = factor(pDE, levels = pDEs))
-tm2 %>% group_by(batch,Tissue) %>%
-    summarise(ng.tot = n(), ng.b = sum(tag.mb==-1), ng.m = sum(tag.mb==1),
-              pg.b = ng.b/ng.tot, pg.m = ng.m/ng.tot) %>%
-    print(n=23)
-summary(tm2$log2mb)
-summary(tm2$log2hm)
-#
-doms = c("BLP", "LP", "PD_L", "MP", "PD_H", "HP", "AHP")
-tm3 = tm2 %>%
-    filter(tag.mb != 0) %>%
-    mutate(tag.lp = ifelse(log2mb > 0, tag.hb, tag.hm),
-           tag.hp = ifelse(log2mb > 0, tag.hm, tag.hb)) %>%
-    mutate(Dom = "MP") %>%
-    mutate(Dom = ifelse(tag.fm == -1 & tag.lp == -1, 'BLP', Dom)) %>%
-    mutate(Dom = ifelse(tag.fm == -1 & tag.lp == 0, 'LP', Dom)) %>%
-    mutate(Dom = ifelse(tag.fm == -1 & tag.lp == 1, 'PD_L', Dom)) %>%
-    mutate(Dom = ifelse(tag.fm == 1 & tag.hp == -1, 'PD_H', Dom)) %>%
-    mutate(Dom = ifelse(tag.fm == 1 & tag.hp == 0, 'HP', Dom)) %>%
-    mutate(Dom = ifelse(tag.fm == 1 & tag.hp == 1, 'AHP', Dom)) %>%
-    mutate(Dom = factor(Dom, levels = doms)) %>%
-    select(Tissue, gid, Dom)
-tm3 %>% dplyr::count(batch, Tissue, Dom) %>% spread(Dom, n) %>% print(n=23)
-#
-doms2 = c("BP", "PL", "AP")
-tm4 = tm2 %>%
-    filter(tag.mb == 0) %>%
-    mutate(hDE = "PL") %>%
-    mutate(hDE = ifelse(tag.fm+tag.hb+tag.hm == -3, "BP", hDE)) %>%
-    mutate(hDE = ifelse(tag.fm+tag.hb+tag.hm == 3, "AP", hDE)) %>%
-    mutate(hDE = factor(hDE, levels = doms2)) %>%
-    select(batch,Tissue, gid, hDE)
-tm4 %>% dplyr::count(batch, Tissue, hDE) %>% spread(hDE, n) %>% print(n=23)
-#
-tx = tm1 %>% left_join(tm2, by = c('batch','Tissue', 'gid')) %>%
-    left_join(tm3, by = c('batch','Tissue', 'gid')) %>%
-    left_join(tm4, by = c('batch','Tissue', 'gid'))
-tx %>% dplyr::count(pDE, Dom, hDE)
-tx
-#}}}
 }
 get_cormatrix <- function(expr) {
     #{{{
